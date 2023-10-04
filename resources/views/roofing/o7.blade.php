2ad1fe303fdf2f59f3d1bd5230aeee7cfc18fbb5
David Niaz <davidniaz@me.com>
2023-10-04 11:14:40 -0500

    Initial commit

---
+++ resources/views/roofing/o7.blade.php
@@ -0,0 +1,1 @@
+Welcome to Roofing o7!
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('redirect-fhh');
+});
+
+Route::get('/roofing/o7', function () {
+    return view('roofing/o7');
+});
