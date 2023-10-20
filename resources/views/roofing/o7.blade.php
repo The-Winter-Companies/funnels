34a3f3cb50ba59e6c3b9b525a2c8b08f8a55a2a0
David Niaz <davidniaz@me.com>
2023-10-20 16:31:20 -0500

    Commit

--- resources/views/roofing/o7.blade.php
+++ resources/views/roofing/o7.blade.php
@@ -1155,8 +1155,6 @@ session_start();
 
     });
 
-
-
 </script>
 <script>
     $(document).ready(function(){
@@ -1305,22 +1303,3 @@ session_start();
 
 </body></html>
 
-<?php
-
-if(isset($_GET['ef_tx_id'])){
-    if(!isset($_GET['ef_aff_id']) && !isset($_GET['ef_offer_id'])){
-        echo "<script>Rollbar.error('New error : Failed accessing the page - quotes.foreverhomehub.com/roofing/o7.' +
- ' Everflow link is incomplete for the click ID [ ' + getUrlParameter('ef_tx_id') +  ' ]! MISSING [ef_offer_id] AND [ef_aff_id]');</script>";
-    }else{
-        if(!isset($_GET['ef_aff_id']) && isset($_GET['ef_offer_id'])){
-            echo "<script>Rollbar.error('New error : Failed accessing the page - quotes.foreverhomehub.com/roofing/o7.' +
- ' Everflow link is incomplete for the click ID [ ' + getUrlParameter('ef_tx_id') +  ' ] and offer ID [ '+ getUrlParameter('ef_offer_id') +' ]! MISSING [ef_aff_id]');</script>";
-        }else{
-            if(!isset($_GET['ef_offer_id']) && isset($_GET['ef_aff_id'])){
-                echo "<script>Rollbar.error('New error : Failed accessing the page - quotes.foreverhomehub.com/roofing/o7.' +
- ' Everflow link is incomplete for the click ID [ ' + getUrlParameter('ef_tx_id') +  ' ] and affiliate ID [ '+ getUrlParameter('ef_aff_id') +' ]! MISSING [ef_offer_id]');</script>";
-            }
-        }
-    }
-}
-?>
--- resources/views/solar/f1.blade.php
+++ resources/views/solar/f1.blade.php
@@ -1204,22 +1204,3 @@ session_start();
 <script type="text/javascript" src="{{ asset('js/power_company-roofing-main.js') }}"></script>
 </body></html>
 
-<?php
-
-if(isset($_GET['ef_tx_id'])){
-    if(!isset($_GET['ef_aff_id']) && !isset($_GET['ef_offer_id'])){
-        echo "<script>Rollbar.error('New error : Failed accessing the page - quotes.foreverhomehub.com/solar/f1.' +
- ' Everflow link is incomplete for the click ID [ ' + getUrlParameter('ef_tx_id') +  ' ]! MISSING [ef_offer_id] AND [ef_aff_id]');</script>";
-    }else{
-        if(!isset($_GET['ef_aff_id']) && isset($_GET['ef_offer_id'])){
-            echo "<script>Rollbar.error('New error : Failed accessing the page - quotes.foreverhomehub.com/solar/f1.' +
- ' Everflow link is incomplete for the click ID [ ' + getUrlParameter('ef_tx_id') +  ' ] and offer ID [ '+ getUrlParameter('ef_offer_id') +' ]! MISSING [ef_aff_id]');</script>";
-        }else{
-            if(!isset($_GET['ef_offer_id']) && isset($_GET['ef_aff_id'])){
-                echo "<script>Rollbar.error('New error : Failed accessing the page - quotes.foreverhomehub.com/solar/f1.' +
- ' Everflow link is incomplete for the click ID [ ' + getUrlParameter('ef_tx_id') +  ' ] and affiliate ID [ '+ getUrlParameter('ef_aff_id') +' ]! MISSING [ef_offer_id]');</script>";
-            }
-        }
-    }
-}
-?>
