9627826eab6ab1d3cacdbf11da8cc6002eac833f
Teodorescu Tudor <tudor@thewintercompanies.com>
2023-11-29 00:23:43 +0200

    added back the fonts

--- resources/views/astrology/index.blade.php
+++ resources/views/astrology/index.blade.php
@@ -54,7 +54,6 @@ session_start();
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@700&family=Poppins:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
-
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('img/favicon-astrology.png')}}">
 
 </head>
